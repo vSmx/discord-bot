b23bcfc11d767a5ea65938f17c1d5f9a74ae3ced
smxoffice <smx@poczta.fm>
2018-04-23 23:36:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,13 @@
+// require
 const Discord = require('discord.js');
+const path = require('path');
+
 const client = new Discord.Client();
 const {BOT_TOKEN, BOT_PREFIX} = process.env;
 
 client.on('ready', () => {
   console.log('Bot został uruchomiony pomyślnie.');
   client.user.setGame(`pomagam ludziom`);
-});
-
-client.on('message', message => {
-  if (message.content === 'ping') message.reply('PONG!');
 });
 
 client.login(BOT_TOKEN);
