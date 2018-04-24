5119a377080ad30953e5a728fa7dacd75ccb3be8
smxoffice <smx@poczta.fm>
2018-04-24 20:57:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,19 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  if (message.content === 'ping') message.reply('pong');
+  if (message.author.bot) return;
+
+  const messageArray = message.content.split(" ");
+	const commandName = messageArray[0];
+	const args = messageArray.slice(1);
+  
+  if (commandName === `${BOT_PREFIX}kostka`) {
+   	const embed = new Discord.RichEmbed()
+		.setColor("#" + ((1 << 24) * Math.random() | 0).toString(16).slice(-6))
+		.setAuthor(message.author.username, message.author.displayAvatarURL)
+		.addField("Losowanie się powiodło", "Wylosowałeś/aś liczbę o numerze: " + (Math.floor(Math.random() * 6) + 1));
+		return message.channel.send(embed); 
+  }
 });
 
 client.login(BOT_TOKEN);
