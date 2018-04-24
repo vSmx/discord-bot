43ee07b5514382247ee50f1905004e0f42fbf02a
smxoffice <smx@poczta.fm>
2018-04-24 20:34:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,23 @@
 // require
+const {BOT_TOKEN, BOT_PREFIX} = process.env;
 const Discord = require('discord.js');
+const BotClient = require('./structures/Client');
 const path = require('path');
 
-const client = new Discord.Client();
-const {BOT_TOKEN, BOT_PREFIX} = process.env;
+const client = new BotClient({
+	commandPrefix: BOT_PREFIX,
+	disableEveryone: true,
+	unknownCommandResponse: false,
+});
 
 client.on('ready', () => {
   console.log('Bot został uruchomiony pomyślnie.');
-  client.user.setGame(`pomagam ludziom`);
+  client.user.setActivity(`ja tu tylko sprzątam`);
+});
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  message.channel.send(`test`);
 });
 
 client.login(BOT_TOKEN);
