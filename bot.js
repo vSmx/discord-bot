c37409f1f2ca8adceb69a4c6a5927c155edd4efa
smxoffice <smx@poczta.fm>
2018-04-25 20:52:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,12 @@ client.on('message', message => {
   
   if (commandName === `${BOT_PREFIX}help`) {
     message.channel.send("komenda w trakcie prac");
+  } else if (commandName === `${BOT_PREFIX}me`) {
+    const embed = new Discord.RichEmbed()
+    .setColor("#" + ((1 << 24) * Math.random() | 0).toString(16).slice(-6))
+    .setAuthor(message.author.username, message.author.displayAvatarURL)
+    .setFooter("crachkouse.pl");
+    return message.channel.send(embed);
   }
 });
 
