849e1c6ec0226d4448097137a7a06ec79829e38f
smxoffice <smx@poczta.fm>
2018-04-26 21:23:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,18 @@
 const {BOT_TOKEN, BOT_PREFIX} = process.env;
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var timestamp = new Date();
 
 client.on('ready', () => {
   console.log('Bot został uruchomiony pomyślnie.');
   client.user.setActivity({
     details: "test",
     state: "test2",
-    timestamp,
     largeImageKey: undefined,
     largeImageText: undefined,
     instance: true,
   });
 });
+asd
 
 client.on('message', message => {
   if (message.author.bot || message.channel.type === "dm") return;
