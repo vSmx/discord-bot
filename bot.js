b6851e0222e473747011fcf4bd8b9dd0f7915cb1
smxoffice <smx@poczta.fm>
2018-04-26 21:43:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,12 +11,10 @@ client.on('message', message => {
   if (message.author.bot || message.channel.type === "dm") return;
   
   const identity = message.guild.members.get(message.author.id);
-  const messageArray = message.content.split(" ");
-  const commandName = messageArray[0];
-  const args = messageArray.slice(1);
+  const command = message.content.split(" ")[0];
   
-  if (commandName === `${BOT_PREFIX}help`) {
-    identity.user.send("test");
+  if (command === `${BOT_PREFIX}help`) {
+    identity.user.send("Wkrótce.");
     message.channel.send("Sprawdź swoje **prywatne wiadomości**, aby uzyskać pomoc!");
   }
 });
